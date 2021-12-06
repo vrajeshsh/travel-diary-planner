3b19d8e0cabb38231817648da0cdf09bbaedae9f
arpitthool <arpitthool1@gmail.com>
2021-12-06 16:12:22 -0500

    changed the galleria captions and other ui changes

--- src/main/java/edu/vt/galleria/PhotoService.java
+++ src/main/java/edu/vt/galleria/PhotoService.java
@@ -31,29 +31,29 @@ public class PhotoService {
         listOfPhotos = new ArrayList<>();
 
         listOfPhotos.add(new Photo("/resources/images/slider/photo1.jpg", "/resources/images/slider/photo1.jpg",
-                "Description for Photo 1", "Health and Fitness are Key to a Happy Life"));
+                "Description for Photo 1", "Travel Diary for all your adventures"));
         listOfPhotos.add(new Photo("/resources/images/slider/photo2.jpg", "/resources/images/slider/photo2.jpg",
-                "Description for Photo 2", "Apple Fitness+ is the first Fitness Experience built around Apple Watch"));
+                "Description for Photo 2", "Store your precious photos, videos and other files as well!"));
         listOfPhotos.add(new Photo("/resources/images/slider/photo3.jpg", "/resources/images/slider/photo3.jpg",
-                "Description for Photo 3", "Body Mass Index for Women"));
+                "Description for Photo 3", "Search hotel all around the world"));
         listOfPhotos.add(new Photo("/resources/images/slider/photo4.jpg", "/resources/images/slider/photo4.jpg",
-                "Description for Photo 4", "Body Mass Index for Men"));
+                "Description for Photo 4", "See current weather at different corners of the world"));
         listOfPhotos.add(new Photo("/resources/images/slider/photo5.jpg", "/resources/images/slider/photo5.jpg",
-                "Description for Photo 5", "Exercise for Good Health and Fitness"));
+                "Description for Photo 5", "Record your travel notes and view them on the go!"));
         listOfPhotos.add(new Photo("/resources/images/slider/photo6.jpg", "/resources/images/slider/photo6.jpg",
-                "Description for Photo 6", "Eat Fruits and Vegetables for Good Health"));
+                "Description for Photo 6", "Tag the location to your travel notes."));
         listOfPhotos.add(new Photo("/resources/images/slider/photo7.jpg", "/resources/images/slider/photo7.jpg",
-                "Description for Photo 7", "A Weight Club Provides Exercise Equipment"));
+                "Description for Photo 7", "Your personal travel diary."));
         listOfPhotos.add(new Photo("/resources/images/slider/photo8.jpg", "/resources/images/slider/photo8.jpg",
-                "Description for Photo 8", "Apps Help You Lose Weight"));
+                "Description for Photo 8", "See future weather predictions to plan your trip accordingly."));
         listOfPhotos.add(new Photo("/resources/images/slider/photo9.jpg", "/resources/images/slider/photo9.jpg",
-                "Description for Photo 9", "Many National Gym Chains are Available for Fitness"));
+                "Description for Photo 9", "Create an account right and start recording your travels!"));
         listOfPhotos.add(new Photo("/resources/images/slider/photo10.jpg", "/resources/images/slider/photo10.jpg",
-                "Description for Photo 10", "Fruits and Vegetables are Key for Good Health and Fitness"));
+                "Description for Photo 10", "Store files just like a Cloud Storage."));
         listOfPhotos.add(new Photo("/resources/images/slider/photo11.jpg", "/resources/images/slider/photo11.jpg",
-                "Description for Photo 11", "Use the Best Fitness Websites"));
+                "Description for Photo 11", "Find the best hotels"));
         listOfPhotos.add(new Photo("/resources/images/slider/photo12.jpg", "/resources/images/slider/photo12.jpg",
-                "Description for Photo 12", "An Apple a Day Keeps the Doctor Away!"));
+                "Description for Photo 12", "Plan your next vacation, right now!                                                                                                                                                                            "));
     }
 
     /*
